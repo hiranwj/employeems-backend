738954159b8176ff7f7ccbfeb6b3dfbdff3440f7
hiranwj <hiranwj.connect@gmail.com>
2023-04-13 11:27:44 +0530

    Add EmployeeRepo in repo package

---
+++ src/main/java/lk/hiranwj/employeeme/repo/EmployeeRepo.java
@@ -0,0 +1,8 @@
+package lk.hiranwj.employeeme.repo;
+
+import lk.hiranwj.employeeme.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
+
+}
