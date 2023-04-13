5813f5cd0071f4f74293ae2cf78bd260a1b1889b
hiranwj <hiranwj.connect@gmail.com>
2023-04-13 12:08:10 +0530

    Add model mapper method in EmployeemeApplication

--- src/main/java/lk/hiranwj/employeeme/EmployeemeApplication.java
+++ src/main/java/lk/hiranwj/employeeme/EmployeemeApplication.java
@@ -1,13 +1,20 @@
 package lk.hiranwj.employeeme;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeemeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeemeApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
