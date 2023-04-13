0b8038850c229277c92149c02bcb0b4b37e0c8fc
hiranwj <hiranwj.connect@gmail.com>
2023-04-13 10:55:43 +0530

    Initial commit

---
+++ src/main/java/lk/hiranwj/employeeme/EmployeemeApplication.java
@@ -0,0 +1,13 @@
+package lk.hiranwj.employeeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/hiranwj/employeeme/EmployeemeApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.hiranwj.employeeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
