889ce7c55c6d7b940f373b791f73d86dbb16dea1
hiranwj <hiranwj.connect@gmail.com>
2023-04-15 19:10:38 +0530

    Add CORS policy annotation in EmployeeController

--- src/main/java/lk/hiranwj/employeeme/controller/EmployeeController.java
+++ src/main/java/lk/hiranwj/employeeme/controller/EmployeeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController // because of REST API
 @RequestMapping("api/v1/employee")
 public class EmployeeController {
